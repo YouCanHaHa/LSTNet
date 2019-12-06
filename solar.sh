9d38d865154dff2ef027603649f58619e21df766
YouCanHaha <charlie.li.xing@gmail.com>
2019-12-06 11:18:12 +0100

    Fix bugs caused by python3 and pytorch1.13

--- solar.sh
+++ solar.sh
@@ -1,1 +1,1 @@
-python main.py --gpu 3 --data data/solar_AL.txt --save save/solar_AL.pt --hidSkip 10 --output_fun Linear
+python3 main.py --gpu 1 --data data/solar_AL.txt --save save/solar_AL.pt --hidSkip 10 --output_fun Linear
